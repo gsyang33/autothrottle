e451a3dd814b75d3df5a150b5990fff3453613b4
kiiimes <dkdla58@gmail.com>
2021-06-03 16:44:40 +0900

    fix bug in run container

--- script/run_container.sh
+++ script/run_container.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 N=`expr 2 \* $1`
-for i in $(seq 1 N)
+for i in $(seq 1 $N)
 do
+	echo $i
 	sudo docker run -idt --name c$i ubuntu:latest
 	sudo docker exec c$i apt update
 	sudo docker exec c$i apt -y install netperf
