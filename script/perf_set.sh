31a5abf9957cd52838589dfd8490fd698f50093d
may21 <may21.lee@gmail.com>
2021-06-03 16:40:33 +0900

    perf_set Mbps unit change

--- script/perf_set.sh
+++ script/perf_set.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 # $1 = conatiner name, $2 = performance goal $3 = the number of vif
 NAME=$1
-GOAL=$2
+D=`expr 1000 \* $1`
+GOAL=`expr $D / 8`
 i=$3
 W=$4
 PID=`docker inspect --format="{{.State.Pid}}" $NAME`
 echo $PID > /proc/oslab/vif$i/pid
 echo $GOAL > /proc/oslab/vif$i/goal
 echo $W > /proc/oslab/vif$i/weight
+#echo $GOAL
